cf6bad5691294d4152044c127494e4f01b4855a5
VISHAl <vp652011@gmail.com>
2026-08-19 17:32:30 +0530

    Add files via upload

---
+++ opretors .py
@@ -0,0 +1,31 @@
+#Comparision Operators
+
+print("Comparision Operators")
+
+a = 13
+b = 15
+
+
+print("01. == Equal:", a==b)
+print("02. != Not Equal:", a!=b)
+print("03. > Greater Than:", a>b)
+print("04. < Less Than:", a<b)
+print("05. <= Less/Equal:", a<=b)
+print("06. >= Greater/Equal:", a>=b)
+
+
+# Logical Operator
+
+print("Logical Operator")
+
+a = True
+b = False
+c = False
+
+print("01.and Logical AND:", a and b)
+print("02. or Logical OR:", a or b)
+print("03. not Logical NOT:",  not b)
+
+
+print ((10>10) or (20<20) and (10>20) or (40<30))
+print ((15>10) or (37>20) and (10<20) or (40>30))
